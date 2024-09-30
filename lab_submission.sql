b25fbcbe63d7b94ecc6a91e6517bf02f01e978bb
IdaAluso <ida.masheti@strathmore.edu>
2024-09-30 21:22:59 +0300

    created a table customer-service_kpi and changed the update function to an insert function

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,4 +1,9 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE TABLE customer_service_kpi (
+customer_service_KPI_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+customer_service_KPI_average_waiting_time_minutes INT NOT NULL,
+PRIMARY KEY (customer_service_KPI_timestamp));
+
 CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
 ON 
 SCHEDULE EVERY 1 HOUR
@@ -6,9 +11,7 @@ STARTS CURRENT_TIMESTAMP
 ON COMPLETION PRESERVE
 COMMENT 'This event computes the total time a customer has waited since they raised a ticket'
 DO
-  UPDATE customer_service_ticket
-  SET 
-    customer_service_total_wait_time_minutes = TIMESTAMPDIFF(MINUTE, customer_service_ticket_raise_time, CURRENT_TIMESTAMP),
-    customer_service_ticket_last_update = CONCAT('The last 1-hour recurring update was made at ', CURRENT_TIMESTAMP)
-  WHERE 
-    customer_service_ticket_resolved = 0;
+   INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)
+    SELECT AVG(TIMESTAMPDIFF(MINUTE, start_time, end_time))
+    FROM customer_service_ticket
+    WHERE end_time > NOW() - INTERVAL 1 HOUR;
